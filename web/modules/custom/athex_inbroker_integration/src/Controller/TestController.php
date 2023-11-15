a288570391152001134638b2e219f0b34f9bca79
Philip Psychias <fpsychias@deloitte.gr>
2023-11-15 18:32:34 +0200

    InBroker ApiDataService & config form

--- web/modules/custom/athex_inbroker_integration/src/Controller/TestController.php
+++ web/modules/custom/athex_inbroker_integration/src/Controller/TestController.php
@@ -2,44 +2,31 @@
 
 namespace Drupal\athex_inbroker_integration\Controller;
 
+use Drupal\athex_inbroker_integration\Service\ApiDataService;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\ClientInterface;
 
 class TestController extends ControllerBase {
+
+	protected $api;
+
 	public function __construct(
-		ClientInterface $http
+		ApiDataService $api
 	) {
-		$this->http = $http;
+		$this->api = $api;
 	}
 
 	public static function create(ContainerInterface $container) {
 		return new static(
-			$container->get('http_client')
+			$container->get('athex_inbroker.api_data')
 		);
-	}
-
-	//TODO: move to service
-	private function callRealtime($transaction, $args = []) {
-		$realtimeDataHost = '';
-		$args += [
-			'userName' => '',
-			'company' => '',
-			'IBSessionId' => '',
-			'lang' => 'GR',
-			'format' => 'json'
-		];
-		$args = http_build_query($args);
-		$get = $this->http->get(
-			$realtimeDataHost . '/' . $transaction . '?' . $args
-		);
-		return json_decode($get->getBody()->getContents(), true);
 	}
 
 	public function test() {
 		return new JsonResponse([
-			'res' => $this->callRealtime('Info', ['code' => 'ETE.ATH'])
+			'res' => $this->api->callRealtime('Info', ['code' => 'ETE.ATH,ALPHA.ATH,TPEIR.ATH,EXAE.ATH'])
 		]);
 	}
 }
