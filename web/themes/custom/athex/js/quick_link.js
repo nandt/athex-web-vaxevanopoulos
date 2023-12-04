643a9a613f1a8d4a2553cea990c722339a290458
vasilis <varapantzis@deloitte.gr>
2023-12-04 17:09:04 +0200

    tiny slider setup + ui controls

---
+++ web/themes/custom/athex/js/quick_link.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function ($) {
+
+var slider;
+
+function initSlider() {
+  slider = tns({
+    container: '.qlWrapper',
+    loop: false,
+    items: 1,
+    nav: false,    
+    autoplay: false,
+    speed: 400,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    preventScrollOnTouch: 'auto',
+    lazyload: true,
+    controlsContainer: "#customize-controls"
+  });
+}
+
+$(window).on("resize load orientationchange", function(){
+  if ($(window).width() < 992) {
+    initSlider();
+  } else {
+    destroySlider();
+  }
+});
+
+function destroySlider() {
+  if (slider){
+    slider.destroy();
+  }
+}
+
+});
+
+
