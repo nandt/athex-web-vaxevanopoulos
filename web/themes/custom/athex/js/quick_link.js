b65de926d3a7b793cfbb88a68be6c791db222123
Philip Psychias <fpsychias@deloitte.gr>
2023-12-13 16:55:44 +0200

    readability fixes for quick_link.js

--- web/themes/custom/athex/js/quick_link.js
+++ web/themes/custom/athex/js/quick_link.js
@@ -1,37 +1,32 @@
 jQuery(document).ready(function ($) {
 
-var slider;
+	var slider;
 
-function initSlider() {
-  slider = tns({
-    container: '.qlWrapper',
-    loop: false,
-    items: 1,
-    nav: false,    
-    autoplay: false,
-    speed: 400,
-    autoplayButtonOutput: false,
-    mouseDrag: true,
-    preventScrollOnTouch: 'auto',
-    lazyload: true,
-    controlsContainer: "#customize-controls"
-  });
-}
+	function initSlider() {
+		slider = tns({
+			container: '.qlWrapper',
+			loop: false,
+			items: 1,
+			nav: false,
+			autoplay: false,
+			speed: 400,
+			autoplayButtonOutput: false,
+			mouseDrag: true,
+			preventScrollOnTouch: 'auto',
+			lazyload: true,
+			controlsContainer: "#customize-controls"
+		});
+	}
 
-$(window).on("resize load orientationchange", function(){
-  if ($(window).width() < 992) {
-    initSlider();
-  } else {
-    destroySlider();
-  }
+	function destroySlider() {
+		if (slider)
+			slider.destroy();
+	}
+
+	$(window).on("resize load orientationchange", function () {
+		if ($(window).width() < 992)
+			initSlider();
+		else
+			destroySlider();
+	});
 });
-
-function destroySlider() {
-  if (slider){
-    slider.destroy();
-  }
-}
-
-});
-
-
