50566cd4bea4c4e57213c773a4d1a2e7b771b268
astathaki <astathaki@deloitte.gr>
2024-01-02 15:01:24 +0200

    Seminar Internal Page

--- web/themes/custom/athex/js/node/seminars.js
+++ web/themes/custom/athex/js/node/seminars.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
-    $('.slider').slick({
-      dots: true,
-      arrows: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
-    });
+ var slider = tns({
+    container: '.my-slider',
+    items: 1.25,
+    mode: 'carousel',
+    slideBy: 'page',
+    autoplay: false,
+    mouseDrag: true,
+    autoplayButtonOutput: false,
+    controls: true,
+    nav: false
+  });
+
+  var controls = document.querySelectorAll('.tns-outer [aria-controls]');
+
+   controls.forEach(function(control) {
+    if (control.getAttribute('data-controls') === 'prev') {
+      control.textContent = '<'; // Σύμβολο για το Prev
+    } else if (control.getAttribute('data-controls') === 'next') {
+      control.textContent = '>'; // Σύμβολο για το Next
+    }
   });
