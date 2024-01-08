e1ece9703b07144c03b821205fb0bfdfb39842ea
astathaki <astathaki@deloitte.gr>
2024-01-08 09:32:46 +0200

    Main menu progress

--- web/themes/custom/athex/js/main_menu.js
+++ web/themes/custom/athex/js/main_menu.js
@@ -1,15 +1,25 @@
 jQuery(document).ready(function ($) {
 
-	$(document).on('click', '.burger-btn', function (e) {
-		$(this).parents('.layout-container').toggleClass('expanded');
-		$(this).parent().siblings('.burger-mobile').toggleClass('mobile-menu-main-expanded');
-		$(this).parents('.main-menu').siblings().children().toggleClass('mobile-menu-main-expanded');
-		// e.target....attributes..["aria-expanded"] = String($(document.body).hasClass("mobile-menu-main-expanded"));
-	});
+    $(document).on('click', '.burger-btn', function () {
+        var $burgerIcon = $(this).find('.burger-icon');
+        var $searchIcon = $('.search-icon');
 
-	$(document).on('click', '.search-icon', function (e) {
-		e.preventDefault();
-		$(this).siblings('div').toggleClass('hidden');
-	})
-})
+        if ($burgerIcon.hasClass('burger-icon')) {
+            $burgerIcon.removeClass('burger-icon').addClass('x-icon');
+            $searchIcon.hide();
+        } else {
+            $burgerIcon.removeClass('x-icon').addClass('burger-icon');
+            $searchIcon.show();
+        }
 
+        $(this).parents('.layout-container').toggleClass('expanded');
+        $(this).parent().siblings('.burger-mobile').toggleClass('mobile-menu-main-expanded');
+        $(this).parents('.main-menu').siblings().children().toggleClass('mobile-menu-main-expanded');
+    });
+
+    $(document).on('click', '.search-icon', function (e) {
+        e.preventDefault();
+        $(this).siblings('div').toggleClass('hidden');
+    });
+
+});
