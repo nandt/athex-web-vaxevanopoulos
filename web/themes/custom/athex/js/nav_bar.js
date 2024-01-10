6cae6fced62d4fc6af12cd7ef7c7b89844d990ef
astathaki <astathaki@deloitte.gr>
2024-01-10 10:45:53 +0200

    Nav Menu debugging

--- web/themes/custom/athex/js/nav_bar.js
+++ web/themes/custom/athex/js/nav_bar.js
@@ -2,11 +2,16 @@ jQuery(document).ready(function ($) {
 
 	$(document).on('click', '.top-level > li', function(e) {
 		$(this).parents('.layout-container').addClass('expanded');
+		$(this).parents('.layout-container').siblings().toggleClass('hidden-menu');
 		$(this).toggleClass('expanded');
 		$(this).siblings().toggleClass('hidden');
 		$(this).siblings().removeClass('expanded');
 		$(this).parents('.main-menu').siblings().toggleClass('hidden');
 		$(this).parent('button').toggleClass('back-button');
+		$('footer').toggleClass('hidden-menu');
+		$('.gradient').toggleClass('hidden-menu');
+		$('.is_Wrapper').addClass('hidden-menu');
+
 	});
 
 	$(document).on('click', '.back', function(e) {
