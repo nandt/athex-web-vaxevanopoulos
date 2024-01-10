6958dd9a8ea1087018db4657cdf1ca47b7dc2556
astathaki <astathaki@deloitte.gr>
2024-01-10 15:11:08 +0200

    Seminar Internal details card fix

--- web/themes/custom/athex/js/node/seminars.js
+++ web/themes/custom/athex/js/node/seminars.js
@@ -14,8 +14,53 @@
 
    controls.forEach(function(control) {
     if (control.getAttribute('data-controls') === 'prev') {
-      control.textContent = '<'; 
+      control.textContent = '<';
     } else if (control.getAttribute('data-controls') === 'next') {
-      control.textContent = '>'; 
+      control.textContent = '>';
     }
   });
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+	const seminarDetails = document.getElementById('seminarDetails');
+	const originalPosition = seminarDetails.getBoundingClientRect().top + window.scrollY;
+
+	window.addEventListener('scroll', () => {
+	  let scrollY = window.scrollY;
+	  if (scrollY >= originalPosition) {
+		seminarDetails.style.position = 'fixed';
+		seminarDetails.style.top = '0';
+	  } else {
+		seminarDetails.style.position = 'relative';
+		seminarDetails.style.top = '';
+	  }
+	});
+  });
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    const seminarDetails = document.getElementById('seminarDetails');
+    const leftSideItems = document.querySelector('.LeftSideItemElements');
+    const originalPosition = seminarDetails.getBoundingClientRect().top + window.scrollY;
+    const additionalOffset = 70;
+
+    window.addEventListener('scroll', () => {
+        let scrollY = window.scrollY;
+        let stopPosition = leftSideItems.getBoundingClientRect().bottom + window.scrollY - seminarDetails.offsetHeight - additionalOffset;
+
+        if (scrollY >= originalPosition && scrollY < stopPosition) {
+            seminarDetails.style.position = 'fixed';
+            seminarDetails.style.top = '0';
+            seminarDetails.style.marginTop = '120px';
+        } else if (scrollY >= stopPosition) {
+            seminarDetails.style.position = 'absolute';
+            seminarDetails.style.top = `${stopPosition - originalPosition}px`;
+            seminarDetails.style.marginTop = '0';
+        } else {
+            seminarDetails.style.position = 'relative';
+            seminarDetails.style.top = '';
+            seminarDetails.style.marginTop = '0';
+        }
+    });
+});
+
+
+
