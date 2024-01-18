91b6416ecfe56a8165391af0483a68e5ce423de7
Giannis Profis <gprofis@deloitte.gr>
2024-01-18 18:24:33 +0200

    finish stock search page

--- web/themes/custom/athex/js/stock_search.js
+++ web/themes/custom/athex/js/stock_search.js
@@ -1,39 +1,6 @@
 jQuery(document).ready(function ($) {
     let stocks =  $('.stocks');
-    stocks.parents().parent().css('overflow-x', 'hidden')
     
     let nav_pills = stocks.find('.nav-pills');
-    nav_pills.addClass('pillsWrapper')
-
-    let pillsSlider;
-
-	function initSlider() {
-		pillsSlider = tns({
-			container: '.pillsWrapper',
-            loop: false,
-            nav: false,
-            autoplay: false,
-            speed: 400,
-            autoplayButtonOutput: false,
-            mouseDrag: true,
-            preventScrollOnTouch: 'auto',
-            lazyload: true,
-            controls:false,
-            responsive: {
-                1: {
-                    items: 5,
-                    slideBy: 4,
-                },
-                370: {
-                    items: 8,
-                    slideBy: 7,
-                },
-                1200: {
-                    items: 27
-                }
-            }
-		})
-
-	}
-	initSlider();
+    nav_pills.addClass('pill-filters')
 })
