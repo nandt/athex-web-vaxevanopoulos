315326a4e65a8ef4bfad4cae95070091064c9289
Philip Psychias <fpsychias@deloitte.gr>
2024-01-22 17:19:33 +0200

    ProductsTable

---
+++ web/modules/custom/athex_d_mde/src/AthexRendering/ProductsTable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\athex_d_mde\AthexRendering;
+
+
+class ProductsTable {
+
+	public array $data;
+
+	public function __construct(array $data = []) {
+		$this->data = $data;
+	}
+
+	public function render() {
+		$rows = array_map(function ($row) {
+			$cols = array_map(function ($d) {
+				return [ 'data' => $d ];
+			}, $row);
+
+			$cols[0]['data'] = [
+				'#type' => 'link',
+				'#title' => $cols[0]['data'],
+				'#url' => \Drupal\Core\Url::fromUri('internal:#')
+			];
+
+			if (count($cols) === 5)
+				$cols[1]['class'] = [ 'mobile-hidden' ];
+
+			return $cols;
+		}, $this->data);
+
+		return [
+			'#type' => 'table',
+			'#attributes' => [
+				'class' => [ 'products-table' ]
+			],
+			'#rows' => $rows
+		];
+	}
+}
