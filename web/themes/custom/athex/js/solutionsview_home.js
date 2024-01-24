22a643267f63bfcd8dcc8116edec285606e94340
Giannis Profis <gprofis@deloitte.gr>
2024-01-24 17:30:24 +0200

    UI and functionality improvements and fixes

--- web/themes/custom/athex/js/solutionsview_home.js
+++ web/themes/custom/athex/js/solutionsview_home.js
@@ -14,7 +14,7 @@ jQuery(document).ready(function ($) {
 			preventScrollOnTouch: 'auto',
 			lazyload: true,
 			controlsPosition: 'bottom',
-			controlsContainer: '#solutions-slider',
+			controlsContainer: '.solutions-slider',
 			responsive: {
 				1: {
 					items: 2,
@@ -30,10 +30,4 @@ jQuery(document).ready(function ($) {
 
 	initSlider();
 
-	const slides = $(document).find('.solviewhomeWrapper').children();
-	slides.each(function(index) {
-		solNumber = index + 1;
-
-		$(this).children('.sol-teaser-number').prepend("test" + solNumber);
-	})
 })
