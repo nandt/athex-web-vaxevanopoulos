74401eddfbc8776fae8116caf55f57b4a6f00851
Giannis Profis <gprofis@deloitte.gr>
2024-01-24 14:17:06 +0200

    Create tile link group and tile link paragraphs

---
+++ web/themes/custom/athex/js/tile_link_slider.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function ($) {
+
+	let tileLinkSlider;
+
+	function initSlider() {
+		tileLinkSlider = tns({
+			container: '.tileLinksWrapper',
+			loop: false,
+			nav: false,
+			autoplay: false,
+			speed: 300,
+			autoplayButtonOutput: false,
+			mouseDrag: true,
+			preventScrollOnTouch: 'auto',
+			lazyload: true,
+			rewind: false,
+			controlsPosition: 'bottom',
+			controlsContainer: '#tiles-slider',
+			responsive: {
+				1: {
+					items: 2,
+					slideBy: 1,
+				},
+				992: {
+					items: 5,
+					slideBy: 1,
+				}
+			}
+		})
+	}
+
+	initSlider();
+})
