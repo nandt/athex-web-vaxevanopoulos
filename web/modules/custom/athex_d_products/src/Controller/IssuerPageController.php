02fa450dcea84b02d5f72ef8a4f82c14544f1a7e
Philip Psychias <fpsychias@deloitte.gr>
2024-01-25 11:46:13 +0200

    ProductPage rendering refactor

--- web/modules/custom/athex_d_products/src/Controller/IssuerPageController.php
+++ web/modules/custom/athex_d_products/src/Controller/IssuerPageController.php
@@ -2,30 +2,26 @@
 
 namespace Drupal\athex_d_products\Controller;
 
+use Drupal\athex_d_products\AthexRendering\ProductPage;
 use Drupal\athex_d_products\ProductType;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Drupal\athex_d_products\Service\IssuerDataService;
-use Drupal\athex_d_products\Service\ProductPageLayoutService;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IssuerPageController extends ControllerBase {
 
-	protected $layout;
 	protected $data;
 
 	public function __construct(
-		ProductPageLayoutService $layout,
 		IssuerDataService $data
 	) {
-		$this->layout = $layout;
 		$this->data = $data;
 	}
 
 	public static function create(ContainerInterface $container) {
 		return new static(
-			$container->get('athex_d_products.layout'),
 			$container->get('athex_d_products.issuer_data')
 		);
 	}
@@ -37,13 +33,11 @@ class IssuerPageController extends ControllerBase {
 			throw new NotFoundHttpException();
 		}
 
-		return $this->layout->render($type, $product_id, [
-			[
-				'#type' => 'container',
-				$this->layout->h2('Profile Section 1'),
-				// ...
-			]
-			// ...
+		$page = new ProductPage([
+			'product_type' => 'stock',
+			'product_id' => $product_id
 		]);
+
+		return $page->render();
 	}
 }
--- web/modules/custom/athex_d_products/src/Controller/StockProfileController.php
+++ web/modules/custom/athex_d_products/src/Controller/StockProfileController.php
@@ -10,36 +10,22 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Drupal\athex_d_products\ProductType;
 use Drupal\athex_d_products\AthexRendering\BsGrid;
-use Drupal\athex_d_products\Service\ProductPageLayoutService;
+use Drupal\athex_d_products\AthexRendering\ProductPage;
 use Drupal\athex_d_products\Service\StockDataService;
 
 
 class StockProfileController extends ControllerBase {
 
-	private static $sectionNames = [
-		'Snapshot' => 'Snapshot',
-		'StockOverview' => 'Stock Overview',
-		'TradingInfo' => 'Trading Information',
-		'KeyStatistics' => 'Key Statistics',
-		'IndexWeight' => 'Index Weight',
-		'HistoricData' => 'Historic Data',
-		'RelatedInstruments' => 'Related Instruments'
-	];
-
-	protected $layout;
 	protected $data;
 
 	public function __construct(
-		ProductPageLayoutService $layout,
 		StockDataService $data
 	) {
-		$this->layout = $layout;
 		$this->data = $data;
 	}
 
 	public static function create(ContainerInterface $container) {
 		return new static(
-			$container->get('athex_d_products.layout'),
 			$container->get('athex_d_products.stock_data')
 		);
 	}
@@ -148,14 +134,11 @@ class StockProfileController extends ControllerBase {
 	}
 
 	private function renderSectionRowSnapshot() {
-		$section = 'Snapshot';
 		return [
 			[
 				'#type' => 'html_tag',
 				'#tag' => 'h2',
-				'#value' => $this->t(
-					self::$sectionNames[$section] ?? $section
-				)
+				'#value' => $this->t('Snapshot')
 			],
 			BsGrid::renderRow([
 				$this->renderSummaryTable(),
@@ -164,48 +147,27 @@ class StockProfileController extends ControllerBase {
 		];
 	}
 
-	private function renderSection(string $section) {
-		$fn = "render{$section}Table";
-
-		if (!method_exists($this, $fn))
-			return [];
-
-		return [
-			[
-				'#type' => 'html_tag',
-				'#tag' => 'h2',
-				'#value' => $this->t(
-					self::$sectionNames[$section] ?? $section
-				)
-			],
-			$this->$fn()
-		];
-	}
-
 	public function render($product_id) {
 		// throw new NotFoundHttpException();
 
-		return $this->layout->render(
-			ProductType::STOCK, $product_id,
-			[
-				BsGrid::renderContainer(
-					$this->renderSectionRowSnapshot()
-				),
-				BsGrid::renderContainer([
-					BsGrid::renderRow([
-						$this->renderSection('StockOverview'),
-						$this->renderSection('TradingInfo'),
-					]),
-					BsGrid::renderRow([
-						$this->renderSection('KeyStatistics'),
-						$this->renderSection('IndexWeight'),
-					]),
-					BsGrid::renderRow([
-						$this->renderSection('HistoricData'),
-						$this->renderSection('RelatedInstruments')
-					])
-				])
-			]
+		$page = new ProductPage([
+			'product_type' => 'stock',
+			'product_id' => $product_id
+		]);
+
+		$page->addContent(
+			BsGrid::renderContainer(
+				$this->renderSectionRowSnapshot()
+			)
 		);
+
+		$page->addSection('Stock Overview', 		$this->renderStockOverviewTable());
+		$page->addSection('Trading Information', 	$this->renderTradingInfoTable());
+		$page->addSection('Key Statistics', 		$this->renderKeyStatisticsTable());
+		$page->addSection('Index Weight', 			$this->renderIndexWeightTable());
+		$page->addSection('Historic Data', 			$this->renderHistoricDataTable());
+		$page->addSection('Related Instruments', 	$this->renderRelatedInstrumentsTable());
+
+		return $page->render();
 	}
 }
