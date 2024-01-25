0ec0067684c96033f18a3224741dcd8ee7d7a0ea
Giannis Profis <gprofis@deloitte.gr>
2024-01-25 17:37:01 +0200

    tile link fixes

--- web/themes/custom/athex/js/tile_link_slider.js
+++ web/themes/custom/athex/js/tile_link_slider.js
@@ -1,33 +1,35 @@
-jQuery(document).ready(function ($) {
-
-	let tileLinkSlider;
-
-	function initSlider() {
-		tileLinkSlider = tns({
-			container: '.tileLinksWrapper',
-			loop: false,
-			nav: false,
-			autoplay: false,
-			speed: 300,
-			autoplayButtonOutput: false,
-			mouseDrag: true,
-			preventScrollOnTouch: 'auto',
-			lazyload: true,
-			rewind: false,
-			controlsPosition: 'bottom',
-			controlsContainer: '.tiles-slider',
-			responsive: {
-				1: {
-					items: 2,
-					slideBy: 1,
-				},
-				992: {
-					items: 5,
-					slideBy: 1,
+(function ($, Drupal, once, tns) {
+	Drupal.behaviors.tinySliderTileLinks = {
+		attach: function(context, settings) {
+			once('tns-tiles-init', '.tl-wrapper', context).forEach(
+				function(sliderNode) {
+					tns({
+						container: $(sliderNode).find('.tileLinksWrapper')[0],
+						loop: false,
+						nav: false,
+						autoplay: false,
+						speed: 300,
+						autoplayButtonOutput: false,
+						mouseDrag: true,
+						preventScrollOnTouch: 'auto',
+						lazyload: true,
+						rewind: false,
+						controlsPosition: 'bottom',
+						controlsContainer: $(sliderNode).find('.tiles-slider')[0],
+						gutter: 8,
+						responsive: {
+							1: {
+								items: 2,
+								slideBy: 1,
+							},
+							992: {
+								items: 5,
+								slideBy: 1,
+							}
+						}
+					})
 				}
-			}
-		})
+			)
+		}
 	}
-
-	initSlider();
-})
+})(jQuery, Drupal, once, tns);
