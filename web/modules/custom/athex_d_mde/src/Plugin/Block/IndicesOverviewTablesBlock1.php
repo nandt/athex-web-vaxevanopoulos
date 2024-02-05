b30e5b256d153ac43c148073d018e7b7b50ba60a
vasileios <vasileios.vaxevanopoulos@gmail.com>
2024-02-05 17:29:36 +0200

    Indices overview Tables todays merket brock uses the config settings from the form

--- web/modules/custom/athex_d_mde/src/Plugin/Block/IndicesOverviewTablesBlock1.php
+++ web/modules/custom/athex_d_mde/src/Plugin/Block/IndicesOverviewTablesBlock1.php
@@ -7,7 +7,8 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\athex_d_mde\Service\IndicesOverviewTablesService;
-use Drupal\athex_sis\Controller;
+use Drupal\Core\Config\ConfigFactoryInterface; // Import the ConfigFactory service
+
 /**
  * Provides a 'IndicesOverviewTables' Block.
  *
@@ -42,12 +43,15 @@ class IndicesOverviewTablesBlock1 extends BlockBase implements ContainerFactoryP
 
 	protected $sisDbDataService;
 	protected $indicesOverviewTablesService;
+	protected $configFactory;
 
-	public function __construct(array $configuration, $plugin_id, $plugin_definition, SisDbDataService $sisDbDataService, IndicesOverviewTablesService $indicesOverviewTablesService)
-	{
+
+	  public function __construct(array $configuration, $plugin_id, $plugin_definition, SisDbDataService $sisDbDataService, IndicesOverviewTablesService $indicesOverviewTablesService, ConfigFactoryInterface $configFactory)
+	  {
 		parent::__construct($configuration, $plugin_id, $plugin_definition);
 		$this->sisDbDataService = $sisDbDataService;
 		$this->indicesOverviewTablesService = $indicesOverviewTablesService;
+		$this->configFactory = $configFactory;
 	}
 
 
@@ -55,13 +59,16 @@ class IndicesOverviewTablesBlock1 extends BlockBase implements ContainerFactoryP
 	 * {@inheritdoc}
 	 */
 	public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+
 	{
 		return new static(
 			$configuration,
 			$plugin_id,
 			$plugin_definition,
 			$container->get('athex_sis.db_data'), // This should be the fourth argument
-			$container->get('athex_d_mde.indices_overview_tables') // This should be the fifth argument
+			$container->get('athex_d_mde.indices_overview_tables'),
+			$container->get('config.factory')
+
 		);
 	}
 
@@ -70,63 +77,16 @@ class IndicesOverviewTablesBlock1 extends BlockBase implements ContainerFactoryP
 	 * {@inheritdoc}
 	 */
 
-	/*public function build()
-	{ $renderArray = [];
 
-		// Assuming $gdValues is defined somewhere or you loop through an array of values
-		$gdValues = ['GD', 'FTSE', 'ETE', 'ALPHA', 'TPEIR', 'EXAE']; // Example GD values
-
-		foreach ($gdValues as $gdValue) {
-			// Define your SQL query within the loop, so it uses the current $gdValue
-			$sql = "SELECT hs.TICKER_EN FROM HELEX_STOCKS hs
-                JOIN HELEX_INDEXCOMPOSITION hic ON hs.STOCK_ID = hic.STOCK_ID
-                JOIN HELEX_INDICES hi ON hic.INDEX_ID = hi.INDEX_ID
-                WHERE hi.TICKER_EN = :gdValue";
-
-			// Fetch data for each GD value using the correct method and parameters
-			$data = $this->sisDbDataService->fetchAllWithParams($sql, [':gdValue' => $gdValue]);
-
-			// Check if $data is an array and not empty before passing to getSubProductsTableRA
-			if (is_array($data) && !empty($data)) {
-				// Use the fetched data in IndicesOverviewTablesService
-				$renderArray[$gdValue] = $this->indicesOverviewTablesService->getSubProductsTableRA($data);
-			}
-		}
-
-		// Assuming you have a template named 'your_custom_block_template' to display this data
-		return $renderArray;
-	}*/
-
-	/*public function build() {
-		$renderArray = [];
-		$gdValues = ['GD', 'FTSE', 'ETE', 'ALPHA', 'TPEIR', 'EXAE'];
-
-		foreach ($gdValues as $gdValue) {
-			$sql = "SELECT hs.TICKER_EN FROM HELEX_STOCKS hs
-        JOIN HELEX_INDEXCOMPOSITION hic ON hs.STOCK_ID = hic.STOCK_ID
-        JOIN HELEX_INDICES hi ON hic.INDEX_ID = hi.INDEX_ID
-        WHERE hi.TICKER_EN = :gdValue";
-
-			$params = [':gdValue' => $gdValue]; // Replace 'someValue' with actual value
-
-			$data = $this->sisDbDataService->fetchAllWithParams($sql, $params);
-
-			if (is_array($data) && !empty($data)) {
-				$renderArray[$gdValue] = $this->indicesOverviewTablesService->getSubProductsTableRA($data); // Assuming 'Risers' is a valid second argument
-			} else {
-				$renderArray[$gdValue] = []; // Ensure an empty array is set if no data
-			}
-		}
-
-		return [
-			'#theme' => 'indices_overview_tables_block1',
-			'#renderArray' => $renderArray,
-		];
-	}*/
 
 	public function build() {
+		$config = $this->configFactory->get('athex_d_mde.settings');
+		$gdValuesString = $config->get('gd_values');
+		$gdValues = $gdValuesString ? explode(',', $gdValuesString) : []; // Use the settings from the config
+
 		$renderArray = [];
-		$gdValues = ['GD', 'FTSE', 'ETE', 'ALPHA', 'TPEIR', 'EXAE'];
+		$allTables = $this->indicesOverviewTablesService->getSubProductsTables();
+
 
 		// Fetch the tables for all categories and GD values
 		$allTables = $this->indicesOverviewTablesService->getSubProductsTables();
