7059255bd4fc04b4d849a5f90f5dcce41d2ef369
vasilis <varapantzis@deloitte.gr>
2024-02-14 12:23:34 +0200

    fixes donut chart

--- web/themes/custom/athex/js/donut_chart.js
+++ web/themes/custom/athex/js/donut_chart.js
@@ -1,62 +1,64 @@
-jQuery(document).ready(function ($) {
+(function ($, Drupal) {
+	Drupal.behaviors.chartCustomBehavior = {
+	  attach: function (context, settings) {
+		var drawChart;
+		var dataArray = [['label', 'value']];
+		var colorArray = [];
 
-    var drawChart;
-    var dataArray = [['label', 'value']];
-    var colorArray = [];
+		google.charts.load("current", { packages: ["corechart"] });
 
-    google.charts.load("current", { packages: ["corechart"] });
+		google.charts.setOnLoadCallback(function () {
+		  drawChart = function (dataArray, colorArray) {
+			var data = google.visualization.arrayToDataTable(dataArray);
 
-    google.charts.setOnLoadCallback(function () {
+			var options = {
+			  pieHole: 0.5,
+			  colors: colorArray,
+			  chartArea: { width: '100%', left: '30%' },
+			  width: '100%'
+			};
 
-        drawChart = function (dataArray, colorArray) {
-            var data = google.visualization.arrayToDataTable(dataArray);
+			var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+			chart.draw(data, options);
+		  }
 
-            var options = {
-                pieHole: 0.5,
-                colors: colorArray,
-                chartArea: { width: '100%', left: '30%' },
-                width: '100%'
-            };
+		  function showChartForDataItem($dataItem) {
+			dataArray = [['label', 'value']];
+			colorArray = [];
 
-            var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-            chart.draw(data, options);
-        }
+			$dataItem.find('.donut_data_item_elements').each(function () {
+			  var label = $(this).find('.ddi_label').text().trim();
+			  var value = parseFloat($(this).find('.ddi_value').text().trim());
+			  var color = $(this).find('.ddi_color').text().trim();
+			  colorArray.push(color);
+			  dataArray.push(['"' + label + '"', value]);
+			});
 
+			drawChart(dataArray, colorArray);
+		  }
 
-        function showChartForDataItem($dataItem) {
-            dataArray = [['label', 'value']];
-            colorArray = [];
+		  var $firstDataItem = $('.donut_data_item:first');
+		  var $firstDataItemLabel = $('.ddi_title:first');
+		  $firstDataItem.addClass('active');
+		  $firstDataItemLabel.addClass('active');
+		  showChartForDataItem($firstDataItem);
 
-            $dataItem.find('.donut_data_item_elements').each(function () {
-                var label = $(this).find('.ddi_label').text().trim();
-                var value = parseFloat($(this).find('.ddi_value').text().trim());
-                var color = $(this).find('.ddi_color').text().trim();
-                colorArray.push(color);
-                dataArray.push(['"' + label + '"', value]);
-            });
+		  $('.ddi_title').click(function () {
+			$('.ddi_title').removeClass('active');
+			$(this).addClass('active');
+			var $dataItem = $(this).closest('.donut_data_item');
 
-            drawChart(dataArray, colorArray);
-        }
+			$('.donut_data_item').removeClass('active');
+			$dataItem.addClass('active');
 
-        var $firstDataItem = $('.donut_data_item:first');
-        var $firstDataItemLabel = $('.ddi_title:first');
-        $firstDataItem.addClass('active');
-        $firstDataItemLabel.addClass('active');
-        showChartForDataItem($firstDataItem);
+			showChartForDataItem($dataItem);
+		  });
 
-        $('.ddi_title').click(function () {
-            $('.ddi_title').removeClass('active');
-            $(this).addClass('active');
-            var $dataItem = $(this).closest('.donut_data_item');
+		  $(window, context).resize(function () {
+			drawChart(dataArray, colorArray);
+		  }).resize(); // Trigger initial resize
 
-            $('.donut_data_item').removeClass('active');
-            $dataItem.addClass('active');
-
-            showChartForDataItem($dataItem);
-        });
-
-        $(window).on('resize orientationchange load', function () {
-            drawChart(dataArray, colorArray);
-        });
-    });
-});
+		});
+	  }
+	};
+  })(jQuery, Drupal);
