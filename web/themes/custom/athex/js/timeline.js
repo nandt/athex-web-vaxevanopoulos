970f980750571c852bd8c236d0a8660f9713ccc9
astathaki <astathaki@deloitte.gr>
2024-02-15 16:39:28 +0200

    Timeline

--- web/themes/custom/athex/js/timeline.js
+++ web/themes/custom/athex/js/timeline.js
@@ -1,141 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.scrollBtn').forEach(function(btn) {
-        btn.addEventListener('click', function() {
-            const targetElement = document.querySelector('#block-athex-content');
-            if (targetElement) {
-                const bottomPosition = targetElement.offsetTop + targetElement.offsetHeight;
-                window.scrollTo({
-                    top: bottomPosition,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-});
-
-jQuery(document).ready(function ($) {
-    const bullets = $('.bullet');
-    const verticalBars = $('.vertical-bar');
-    const sections = $('.section');
-    const lastVerticalBar = $('.vertical-bar.last');
-
-    setVerticalBarHeights();
-
-    $(window).on('resize', setVerticalBarHeights);
-
-    $(document).on('scroll', function () {
-        let currentSectionIndex = 0;
-
-        sections.each(function (index) {
-            const rect = $(this).get(0).getBoundingClientRect();
-
-            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-                currentSectionIndex = index;
-            }
-        });
-
-        setActive(currentSectionIndex);
-    });
-
-    function scrollToSection(index) {
-        sections.eq(index).get(0).scrollIntoView({ behavior: 'smooth' });
-    }
-
-    bullets.on('click', function () {
-        const index = $(this).data('index');
-        scrollToSection(index);
-    });
-
-    function setActive(index) {
-        bullets.each(function (i) {
-            $(this).toggleClass('active', i === index);
-        });
-
-        verticalBars.each(function (i) {
-            $(this).toggleClass('active', i === index);
-        });
-        lastVerticalBar.css('opacity', index === sections.length - 1 ? 0 : 1);
-    }
-
-    function setVerticalBarHeights() {
-        verticalBars.each(function (i) {
-            const sectionHeight = sections.eq(i+1).outerHeight();
-            $(this).css('height', sectionHeight - 38);
-        });
-    }
-
-    function getAndDisplayMarginRight() {
-        var marginRight = $('.container').css('margin-right');
-        var negativeMarginRight = '-' + marginRight;
-        $('.logo-wrapper').css('margin-right', negativeMarginRight);
-      }
-
-      // On document ready
-      $(document).ready(function() {
-        getAndDisplayMarginRight(); // Initial call on page load
-
-        // On window resize
-        $(window).resize(function() {
-          getAndDisplayMarginRight();
-        });
-
-
-        $(window).on('orientationchange', function() {
-          getAndDisplayMarginRight();
-        });
-
-        $(window).on('load', function() {
-            getAndDisplayMarginRight();
-          });
-      });
-});
-
+let lastScrollTop = 0;
 
 window.addEventListener('scroll', function() {
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const verticalBars = document.querySelectorAll('.vertical-bar');
     const bullets = document.querySelectorAll('.bullet');
-    const triggerHeight = window.innerHeight * 0.75; // ή όποιο σημείο θέλετε
 
-    bullets.forEach(function(bullet) {
-        const bulletPosition = bullet.getBoundingClientRect().top;
 
-        if (bulletPosition < triggerHeight) {
+    const triggerPoint = window.innerHeight * 0.8;
+
+
+    verticalBars.forEach((bar) => {
+        const barPosition = bar.getBoundingClientRect().top + bar.offsetHeight / 2;
+        if (barPosition < triggerPoint) {
+            bar.classList.add('scrolled');
+        } else if (currentScrollTop < lastScrollTop && barPosition >= triggerPoint) {
+            bar.classList.remove('scrolled');
+        }
+    });
+
+
+    bullets.forEach((bullet) => {
+        const bulletPosition = bullet.getBoundingClientRect().top + bullet.offsetHeight / 2;
+        if (bulletPosition < triggerPoint) {
             bullet.classList.add('bullet-active');
-        } else {
+        } else if (currentScrollTop < lastScrollTop && bulletPosition >= triggerPoint) {
             bullet.classList.remove('bullet-active');
         }
     });
+
+    lastScrollTop = currentScrollTop;
 });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var btnShare = document.querySelector('.btnShare');
-    var bottomCTAs = document.querySelector('.bottomCTAs');
-
-    var observer = new IntersectionObserver(function(entries) {
-
-        if(entries[0].intersectionRatio === 0) {
-            bottomCTAs.style.display = 'flex';
-        } else {
-            bottomCTAs.style.display = 'none';
-        }
-    }, { threshold: [0] });
-
-    observer.observe(btnShare);
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener('scroll', function() {
-        const verticalBar = document.querySelector('.vertical-bar');
-        if (window.scrollY > 50) { // Αντικαταστήστε το 50 με το ύψος στο οποίο θέλετε να αλλάξει το χρώμα
-            verticalBar.classList.add('scrolled');
-        } else {
-            verticalBar.classList.remove('scrolled');
-        }
-    });
-});
-
-
-
-
